8c4ce5d65520f281b477fe5adddaa4d7dded41a7
laurencik.a@gmail.com <970520/4960>
2016-06-05 20:37:45 +0200

    Pridanie Zobrazenia kategorii

--- app/src/main/java/cz/muni/fi/pv239/androidpoll/Activities/MenuActivity.java
+++ app/src/main/java/cz/muni/fi/pv239/androidpoll/Activities/MenuActivity.java
@@ -1,5 +1,6 @@
 package cz.muni.fi.pv239.androidpoll.Activities;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -13,7 +14,11 @@ import java.util.List;
 
 import cz.muni.fi.pv239.androidpoll.Adapters.CategoryAdapter;
 import cz.muni.fi.pv239.androidpoll.Entities.Category;
+import cz.muni.fi.pv239.androidpoll.Managers.impl.CategoryManagerImpl;
+import cz.muni.fi.pv239.androidpoll.Managers.interfaces.CategoryManager;
 import cz.muni.fi.pv239.androidpoll.R;
+import cz.muni.fi.pv239.androidpoll.ServerConnection.ServerResponse;
+import rx.Observer;
 
 /**
  * Created by Filip on 28.5.2016.
@@ -22,29 +27,47 @@ public class MenuActivity extends AppCompatActivity {
 
     private GridView gridCategories;
     private CategoryAdapter categoryAdapter;
+    private Context that=this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-
-        gridCategories = (GridView) findViewById(R.id.categoriesGridView);
-        List<Category> categories = getCategories();
-
-        categoryAdapter = new CategoryAdapter(this, categories);
-        gridCategories.setAdapter(categoryAdapter);
-        gridCategories.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        Observer<ServerResponse<List<Category>>> observer = new Observer<ServerResponse<List<Category>>>() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                onCategoryClick((Category) categoryAdapter.getItem(position));
-            }
-        });
-        categoryAdapter.setOnItemClickListener(new AdapterView.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onCategoryClick(v);///get info about category to open right poll here
-            }
-        });
+            public void onCompleted() {
 
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(ServerResponse<List<Category>> response) {
+                if(response.isSuccessful()){
+                    gridCategories = (GridView) findViewById(R.id.categoriesGridView);
+                    List<Category> categories = response.getData();
+
+                    categoryAdapter = new CategoryAdapter(that, categories);
+                    gridCategories.setAdapter(categoryAdapter);
+                    gridCategories.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                            onCategoryClick((Category) categoryAdapter.getItem(position));
+                        }
+                    });
+                    categoryAdapter.setOnItemClickListener(new AdapterView.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            onCategoryClick(v);///get info about category to open right poll here
+                        }
+                    });
+                }
+            }
+        };
+        CategoryManager manager= new CategoryManagerImpl();
+        manager.getAllCategories(observer);
     }
     public List<Category> getCategories(){
         List<Category> categories = new ArrayList<>();
