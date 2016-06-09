0f29936a5ce518d38a3c5199afca2632658124ba
marcus991 <marek.janco123@gmail.com>
2016-06-09 17:28:43 +0200

    skip so swipom

--- app/src/main/java/cz/muni/fi/pv239/androidpoll/Activities/AnswerActivity.java
+++ app/src/main/java/cz/muni/fi/pv239/androidpoll/Activities/AnswerActivity.java
@@ -7,9 +7,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.github.mikephil.charting.data.Entry;
 
@@ -17,6 +21,7 @@ import java.net.UnknownHostException;
 import java.util.List;
 
 import cz.muni.fi.pv239.androidpoll.Adapters.AnswerAdapter;
+import cz.muni.fi.pv239.androidpoll.Entities.Answer;
 import cz.muni.fi.pv239.androidpoll.Entities.Category;
 import cz.muni.fi.pv239.androidpoll.Entities.Option;
 import cz.muni.fi.pv239.androidpoll.Entities.Question;
@@ -37,15 +42,16 @@ public class AnswerActivity extends AppCompatActivity {
 
     private Question question;
     private List<Option> options;
+    private Category category = new Category();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_answer);
+        RelativeLayout rl = (RelativeLayout) findViewById(R.id.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        final Category category = new Category();
-        category.setName(this.getIntent().getStringExtra("Category.name"));
-        category.setId(this.getIntent().getLongExtra("Category.id", 0));
+        this.category.setName(this.getIntent().getStringExtra("Category.name"));
+        this.category.setId(this.getIntent().getLongExtra("Category.id", 0));
         toolbar.setTitle(category.getName()); // CATEGORY NAME
         QuestionManager manager= new QuestionManagerImpl();
         final Observer<ServerResponse<Question>> observer= new Observer<ServerResponse<Question>>() {
@@ -86,7 +92,15 @@ public class AnswerActivity extends AppCompatActivity {
             @Override
             public void onNext(final ServerResponse<Question> questionServerResponse) {
                 if(!questionServerResponse.isSuccessful()){
+
+
+
+
+
                     AnswerActivity.this.setContentView(R.layout.content_answer_empty);
+                    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+                    toolbar.setTitle(category.getName()); // CATEGORY NAME
+
                 }else {
 
                     OptionManager optionManger = new OptionManagerImpl();
@@ -118,6 +132,81 @@ public class AnswerActivity extends AppCompatActivity {
         manager.getRandomQuestion(observer, category,
                 SharedPrefsContainer.getSharedPreferences(that).getString("username", ""),
                 SharedPrefsContainer.getSharedPreferences(that).getString("password", ""));
+
+        rl.setOnTouchListener(new View.OnTouchListener() {
+            private int min_distance = 100;
+            private float downX, downY, upX, upY;
+            View v;
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                this.v = v;
+                switch (event.getAction()) { // Check vertical and horizontal touches
+                    case MotionEvent.ACTION_DOWN: {
+                        downX = event.getX();
+                        downY = event.getY();
+                        return true;
+                    }
+                    case MotionEvent.ACTION_UP: {
+                        upX = event.getX();
+                        upY = event.getY();
+
+                        float deltaX = downX - upX;
+                        float deltaY = downY - upY;
+
+                        //HORIZONTAL SCROLL
+                        if (Math.abs(deltaX) > Math.abs(deltaY)) {
+                            if (Math.abs(deltaX) > min_distance) {
+                                // left or right
+                                if (deltaX < 0) {
+                                    this.onLeftToRightSwipe();
+                                    return true;
+                                }
+                                if (deltaX > 0) {
+                                    this.onRightToLeftSwipe();
+                                    return true;
+                                }
+                            } else {
+                                //not long enough swipe...
+                                return false;
+                            }
+                        }
+                        //VERTICAL SCROLL
+                        else {
+                            if (Math.abs(deltaY) > min_distance) {
+                                // top or down
+                                if (deltaY < 0) {
+                                    this.onTopToBottomSwipe();
+                                    return true;
+                                }
+                                if (deltaY > 0) {
+                                    this.onBottomToTopSwipe();
+                                    return true;
+                                }
+                            } else {
+                                //not long enough swipe...
+                                return false;
+                            }
+                        }
+                        return false;
+                    }
+                }
+                return false;
+            }
+
+            public void onLeftToRightSwipe() {
+            }
+
+            public void onRightToLeftSwipe() {
+                onSkipClick(null);
+            }
+
+            public void onTopToBottomSwipe() {
+            }
+
+            public void onBottomToTopSwipe() {
+            }
+        });
     }
 
     public void onSkipClick(View v){
@@ -163,8 +252,10 @@ public class AnswerActivity extends AppCompatActivity {
 
             }
         };
-        manager.skipQuestion(observer,question,SharedPrefsContainer.getSharedPreferences(that).getString("username",null),SharedPrefsContainer.getSharedPreferences(that).getString("password",null));
+        manager.skipQuestion(observer, question, SharedPrefsContainer.getSharedPreferences(that).getString("username", null), SharedPrefsContainer.getSharedPreferences(that).getString("password", null));
         Intent intent = new Intent(AnswerActivity.this, AnswerActivity.class);
+        intent.putExtra("Category.name",this.category.getName());
+        intent.putExtra("Category.id", this.category.getId());
         startActivity(intent);
     }
 
@@ -175,4 +266,5 @@ public class AnswerActivity extends AppCompatActivity {
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
     }
+
 }
